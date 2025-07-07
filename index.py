9b434b149f3e27774eaef32c34638edadda32844
yousefyaseer337 <yousefyasser747@gmail.com>
2025-07-07 05:38:23 +0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,56 @@
+print("hello python") # string
+
+print(10) # integer
+
+print(2.4) #float 
+
+print(7*8)
+
+name = "mohmmed"
+print(name)
+print(type(name))
+
+num1 = 12
+num2 = 16 
+
+print(num1 + num2)
+
+print(num1*num2)
+print(num1/num2)
+
+user_name = input("enter ypur name : ")
+phone_number = input("ener your phone number : ")
+
+print(user_name,phone_number)
+
+string = "python"
+
+integer = 100
+
+floating = 7.4 
+
+
+# data struchrd
+
+list1 = ["python" , "html" , 12,3 ,3.5,True, [1,2] ]
+
+tuple1 = ("php" , "css" , 12,3 ,3.5,True, [1,2] , (1,2)) 
+
+set1 = {"python" , "html" , 12,3 ,3.5,True,  (1,2) }
+
+dictionry = {
+    "one" : 1,
+    "two" : 2,
+    1     : "one" , 
+    2.3   : "float" ,
+    (1,2) : "tuple",
+    "list": [1,2] ,
+    "set" : {3,4}  , 
+    "dict": {2:4}
+}
+
+
+print(string,list1,tuple1,set1 )
+print(dictionry)
+
+
